72b339ca8d72ffdba074b10a5859688700a7459b
Mark Buckle <markbuckle92@gmail.com>
2024-06-14 09:08:25 -0230

    Graph using react-native-graph

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,5 @@
+// Consider this the entry point in the application. This is the root layout, or the layout that will be wrapped around all screens in the application
+
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
   DarkTheme,
@@ -9,6 +11,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { useColorScheme } from "@/src/components/useColorScheme";
 
@@ -53,11 +56,13 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
---
+++ src/components/Graph.tsx
@@ -0,0 +1,50 @@
+import { View, Text } from "./Themed";
+import { LineGraph, GraphPoint } from "react-native-graph";
+import Colors from "../constants/Colors";
+import timeseries from "@/assets/data/timeseries.json";
+import { MonoText } from "./StyledText";
+import { useState } from "react";
+
+const Graph = () => {
+  const points: GraphPoint[] = timeseries.values.map((value) => ({
+    date: new Date(value.datetime),
+    value: Number.parseFloat(value.close),
+  }));
+
+  const [selectedPoint, setSelectedPoint] = useState<GraphPoint>(
+    points[points.length - 1]
+  );
+
+  //   You can use the console.log below to see if the graphs data points are correct
+  //   console.log(JSON.stringify(graphPoints, null, 2));
+
+  const onPointSelected = (point: GraphPoint) => {
+    setSelectedPoint(point);
+  };
+
+  return (
+    <View>
+      <MonoText style={{ fontSize: 20, fontWeight: "bold", color: "green" }}>
+        ${selectedPoint?.value.toFixed(1)}
+      </MonoText>
+      <Text style={{ color: "gray" }}>
+        {selectedPoint?.date.toDateString()}
+      </Text>
+
+      <LineGraph
+        style={{ width: "100%", height: 300 }}
+        points={points}
+        animated={true}
+        color="#017560"
+        gradientFillColors={["#0175605D", "#7476df00"]}
+        enablePanGesture
+        onPointSelected={onPointSelected}
+        enableIndicator
+        indicatorPulsating
+        enableFadeInMask
+      />
+    </View>
+  );
+};
+
+export default Graph;
